758b8dec7db510e30bf40eb6905ab8b047063064
AlekseyAmp <thientaovv@gmail.com>
2023-03-19 21:10:46 +0500

    deletes import's

--- server/src/auth/oauth2.py
+++ server/src/auth/oauth2.py
@@ -1,10 +1,7 @@
 import base64
 from typing import List
-from fastapi import Depends, HTTPException, status
 from fastapi_jwt_auth import AuthJWT
 from pydantic import BaseModel
-
-from models.user import User
 
 from config.settings import settings
 
@@ -13,7 +10,6 @@ class Settings(BaseModel):
     authjwt_decode_algorithms: List[str] = [settings.JWT_ALGORITHM]
     authjwt_token_location: set = {'cookies', 'headers'}
     authjwt_access_cookie_key: str = 'access_token'
-    authjwt_refresh_cookie_key: str = 'refresh_token'
     authjwt_cookie_csrf_protect: bool = False
     authjwt_public_key: str = base64.b64decode(
         settings.JWT_PUBLIC_KEY).decode('utf-8')
@@ -24,40 +20,3 @@ class Settings(BaseModel):
 @AuthJWT.load_config
 def get_config():
     return Settings()
-
-class NotVerified(Exception):
-    pass
-
-
-class UserNotFound(Exception):
-    pass
-
-
-async def require_user(Authorize: AuthJWT = Depends()):
-    try:
-        Authorize.jwt_required()
-        user_id = Authorize.get_jwt_subject()
-
-        user = await User.get(str(user_id))
-
-        if not user:
-            raise UserNotFound('Пользователь больше не существует')
-
-        # if not user["verified"]:
-        #     raise NotVerified('You are not verified')
-
-    except Exception as e:
-        error = e.__class__.__name__
-        print(e)
-        if error == 'MissingTokenError':
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail='Вы не авторизованы')
-        if error == 'UserNotFound':
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail='Пользователь больше не существует')
-        if error == 'NotVerified':
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail='Пожалуйста, подтвердите свой аккаунт')
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='Токен недействителен или просрочен')
-    return user_id
