44136dda583264a8fb9e086ef751a210662ad330
AlekseyAmp <thientaovv@gmail.com>
2023-03-19 21:12:08 +0500

    add new connect to GridFS

--- server/src/config/database.py
+++ server/src/config/database.py
@@ -1,57 +1,26 @@
+from pymongo import MongoClient
+from gridfs import GridFS
 from config.settings import settings
-
-from beanie import init_beanie, PydanticObjectId
 from models.user import User
+from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseSettings, BaseModel
+from pydantic import BaseSettings
 
 
 class DbSettings(BaseSettings):
     async def initialize_database(self):
         client = AsyncIOMotorClient(settings.DATABASE_URL)
-        await init_beanie(database=client.get_default_database(),
-                          document_models=[User])
+        database = client[settings.DATABASE_NAME]
+        await init_beanie(database=database, document_models=[User])
+
+    async def close_connection(self):
+        self.fs.close()
+        self.fs = None
 
     class Config:
         env_file = ".env"
 
-
-class Database:
-    def __init__(self, model):
-        self.model = model
-
-    async def save(self, document):
-        await document.create()
-        return
-
-    async def get(self, id: PydanticObjectId):
-        doc = await self.model.get(id)
-        if doc:
-            return doc
-        return False
-
-    async def get_all(self):
-        docs = await self.model.find_all().to_list()
-        return docs
-
-    async def update(self, id: PydanticObjectId, body: BaseModel):
-        doc_id = id
-        des_body = body.dict()
-
-        des_body = {k: v for k, v in des_body.items() if v is not None}
-        update_query = {"$set": {
-            field: value for field, value in des_body.items()
-        }}
-
-        doc = await self.get(doc_id)
-        if not doc:
-            return False
-        await doc.update(update_query)
-        return doc
-
-    async def delete(self, id: PydanticObjectId):
-        doc = await self.get(id)
-        if not doc:
-            return False
-        await doc.delete()
-        return True
+class GridFSSettings():
+    mongo_client = MongoClient(settings.DATABASE_URL)
+    db = mongo_client[settings.DATABASE_NAME]
+    file = GridFS(db)
