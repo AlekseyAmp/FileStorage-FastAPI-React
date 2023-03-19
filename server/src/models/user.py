1d8eabef1d1b133e09ba3c672a38f352690e5d35
AlekseyAmp <thientaovv@gmail.com>
2023-03-19 21:13:24 +0500

    add new fields in user

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -1,6 +1,6 @@
+
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
-from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
 from beanie import Document
 
 class Register(BaseModel):
@@ -13,16 +13,26 @@ class Login(BaseModel):
     password: str
 
 
-class UserResponse(BaseModel):
-    email: EmailStr
-
-
-
 class User(Document):
-
-    class Settings:
-        name = "users"
 
     email: EmailStr
     password: str
-    created_at: Optional[datetime] = None
+    created_at: datetime = datetime.now
+    storage_used: float = Field(default=0, ge=0, lt=15)
+    max_storage: float = Field(default=15, ge=0, le=15)
+    is_premium: bool = Field(default=False)
+
+    class Settings:
+        schema_extra = {
+            "example": {
+                "email": "example@example.com",
+                "password": "example_password",
+                "created_at": "2022-01-01T12:00:00",
+                "storage_used": 0,
+                "max_storage": 15.0,
+                "is_premium": True
+            }
+        }
+
+
+
