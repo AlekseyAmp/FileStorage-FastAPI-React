208a472557efa3d06c1d6c470c102984d4d24a47
AlekseyAmp <thientaovv@gmail.com>
2023-03-22 08:11:35 +0500

    add new info in every category, now each category will display its own formats file

--- client/src/pages/Categories/Music.jsx
+++ client/src/pages/Categories/Music.jsx
@@ -5,6 +5,8 @@ import Category from '../../components/Category/Category'
 function Music() {
   return (
     <Category
+    categoryName={'music'}
+    imagePath='../../img/categories/music.png'
      title={'Музыка'}
      labelTitle = {'музыке'}
      background='rgb(220 227 245 / 60%)'
--- client/src/pages/Categories/Videos.jsx
+++ client/src/pages/Categories/Videos.jsx
@@ -5,6 +5,8 @@ import Category from '../../components/Category/Category'
 function Videos() {
   return (
     <Category
+    categoryName={'videos'}
+    imagePath='../../img/categories/videos.png'
     title={'Видео'}
     labelTitle = {'видео'}
     background='rgb(220 227 245 / 60%)'
