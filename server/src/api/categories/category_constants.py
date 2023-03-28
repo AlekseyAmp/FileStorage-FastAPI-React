e885781e4f014cb117e8fb114af01340062d01d0
AlekseyAmp <thientaovv@gmail.com>
2023-03-28 20:43:40 +0500

    add .txt

--- server/src/api/categories/category_constants.py
+++ server/src/api/categories/category_constants.py
@@ -1,5 +1,5 @@
 ALLOWED_FORMATS = {
-    'documents': {'pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'},
+    'documents': {'pdf','txt', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'},
     'images': {'jpeg', 'png', 'gif', 'tiff', 'psd', 'eps', 'ai', 'indd', 'raw', 'jpg', 'webp', 'apng', 'avif', 'svg', 'bmp', 'ico'},
     'videos': {'mp4', 'avi', 'mov', 'wmv'},
     'music': {'mp4', 'avi', 'mov', 'wmv'},
