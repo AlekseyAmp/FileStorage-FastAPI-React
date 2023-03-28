30e48f4f2d72974d6c23667d708bff5f37bef3a3
AlekseyAmp <thientaovv@gmail.com>
2023-03-28 20:45:18 +0500

    add rename function

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,25 +1,70 @@
 import React, { useState, useEffect } from 'react';
-import axios from '../../axios'
+import axios from '../../axios';
 import Cookie from 'js-cookie';
 
 import '../../assets/variables.scss';
 
 import File from '../File/File';
-import SearchInput from '../SearchInput/SearchInput'
-import styles from './Category.module.scss'
+import SearchInput from '../SearchInput/SearchInput';
+import styles from './Category.module.scss';
 
 function Category(props) {
-
   const { categoryName, imagePath, title, titleIcon, labelTitle, background } = props;
 
   const categoryStyle = {
-    background: background
+    background: background,
   };
 
   const [files, setFiles] = useState([]);
   const [showContextMenu, setShowContextMenu] = useState(false);
-  const access_token = Cookie.get('access_token');
   const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
+  const [selectedFile, setSelectedFile] = useState(null);
+  const access_token = Cookie.get('access_token');
+
+
+  async function handleContextMenu(e, file) {
+    e.preventDefault();
+    setSelectedFile(file);
+    setShowContextMenu(true);
+    setContextMenuPosition({ x: e.pageX, y: e.pageY });
+  }
+
+
+  async function handleCloseContextMenu() {
+    setShowContextMenu(false);
+  }
+
+  async function handleRename() {
+    const newName = 'test';
+    try {
+      const response = await axios.patch(`rename_file/${selectedFile.file_id}/${newName}`, newName, {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
+      });
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.response.data.detail);
+    }
+  }
+
+  async function handleDownload() {
+    try {
+      const response = await axios.get(`download_file/${selectedFile.file_id}`, {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
+      });
+      const url = window.URL.createObjectURL(new Blob([response.data], { type: selectedFile.content_type }));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', selectedFile.filename);
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.log(error.response.data.detail);
+    }
+  }
 
 
   useEffect(() => {
@@ -27,29 +72,17 @@ function Category(props) {
       try {
         const response = await axios.get(`/category/${categoryName}`, {
           headers: {
-            'Authorization': `Bearer ${access_token}`
-          }
+            Authorization: `Bearer ${access_token}`,
+          },
         });
         setFiles(response.data.files);
       } catch (error) {
-        console.error(error);
+        console.log(error.response.data.detail);
       }
-    };
+    }
     getCategoryFiles();
   }, []);
 
-
-
-    async function handleContextMenu(e) {
-      e.preventDefault();
-      setShowContextMenu(true);
-      setContextMenuPosition({ x: e.pageX, y: e.pageY });
-    };
-
-
-    async function handleCloseContextMenu() {
-      setShowContextMenu(false);
-    };
 
   return (
     <div className={styles.category} style={categoryStyle} onClick={handleCloseContextMenu}>
@@ -61,24 +94,22 @@ function Category(props) {
         <SearchInput title={`Поиск по ${labelTitle}`} />
 
         <div className={styles.files}>
-
           {files.map((file) => (
             <File
               key={file.file_id}
-              onContextMenu={handleContextMenu}
+              onContextMenu={(e) => handleContextMenu(e, file)}
               image={`../img/categories/${imagePath}`}
               filename={file.filename}
               fileformat={file.fileformat}
-              size={Math.floor(file.size / 1000) + ' КБ'}
+              size={`${Math.floor(file.size / 1000)} КБ`}
             />
           ))}
-
         </div>
 
         {showContextMenu && (
           <div className={styles.contextMenu} onClick={handleCloseContextMenu} style={{ left: contextMenuPosition.x, top: contextMenuPosition.y }}>
-            <div className={styles.contextMenuItem}>Скачать</div>
-            <div className={styles.contextMenuItem}>Переименовать</div>
+            <div className={styles.contextMenuItem} onClick={handleDownload}>Скачать</div>
+            <div className={styles.contextMenuItem} onClick={handleRename}>Переименовать</div>
             <div className={styles.contextMenuItem}>Поделиться</div>
             <div className={styles.contextMenuItem}>Добавить в избранное</div>
             <div className={styles.contextMenuItem}>Удалить</div>
