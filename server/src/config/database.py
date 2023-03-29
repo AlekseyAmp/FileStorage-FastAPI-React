8f44d9be59d07a62ac91f1a84b19593a63cd1677
AlekseyAmp <thientaovv@gmail.com>
2023-03-29 10:15:48 +0500

    delete gridfs connect

--- server/src/config/database.py
+++ server/src/config/database.py
@@ -1,11 +1,8 @@
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
+from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from pydantic import BaseSettings
 
-from pymongo import MongoClient
-from gridfs import GridFS, GridFSBucket
-
-from models.user import User
+from models import user, file
 from config.settings import settings
 
 
@@ -13,14 +10,7 @@ class DbSettings(BaseSettings):
     async def initialize_database(self):
         client = AsyncIOMotorClient(settings.DATABASE_URL)
         database = client[settings.DATABASE_NAME]
-        await init_beanie(database=database, document_models=[User])
+        await init_beanie(database=database, document_models=[user.User, file.File])
 
     class Config:
         env_file = ".env"
-
-
-class GridFSSettings():
-    mongo_client = MongoClient(settings.DATABASE_URL)
-    db = mongo_client[settings.DATABASE_NAME]
-    file = GridFS(db)
-    file_bucket = GridFSBucket(db)
