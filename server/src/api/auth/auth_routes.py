cc1476387cdf2536858bbfe0689f145dca089fda
AlekseyAmp <thientaovv@gmail.com>
2023-03-29 10:12:50 +0500

    add auth folder in api

---
+++ server/src/api/auth/auth_routes.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Response, status, Depends
+
+from models.user import Login, Register
+from api.auth.jwt_confg import AuthJWT
+from api.users.user_services import get_user_id
+from api.auth.auth_services import create_new_user, login_user, refresh_access_token, logout_user
+
+
+router = APIRouter()
+
+
+@router.post('/register')
+async def register(credentials: Register):
+    return await create_new_user(credentials)
+
+
+@router.post('/login')
+async def login(credentials: Login, response: Response, Authorize: AuthJWT = Depends()):
+    return await login_user(credentials, response, Authorize)
+
+
+@router.get('/refresh')
+async def refresh_token(response: Response, Authorize: AuthJWT = Depends(), user_id: get_user_id = Depends()):
+    return await refresh_access_token(Authorize, response, str(user_id))
+
+
+@router.get('/logout', status_code=status.HTTP_200_OK)
+async def logout(response: Response, Authorize: AuthJWT = Depends()):
+    return await logout_user(response, Authorize)
---
+++ server/src/api/auth/auth_utils.py
@@ -0,0 +1,20 @@
+from passlib.context import CryptContext
+import re
+
+
+def is_valid_email(email: str) -> bool:
+    pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+    if re.match(pat, email):
+        return True
+    return False
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
