aae3ff1c0f9f8e87b87f29a158a3d53b45253a8f
AlekseyAmp <thientaovv@gmail.com>
2023-03-29 10:13:56 +0500

    make async functions

--- server/src/api/files/file_routes.py
+++ server/src/api/files/file_routes.py
@@ -2,32 +2,38 @@
 from fastapi import APIRouter, Depends, UploadFile
 
 from api.users.user_services import get_user_id
-from api.files.file_services import upload_file, download_file, rename_file, get_all_files, in_basket_file
+from api.files.file_services import upload_file, download_file, rename_file, delete_file, in_basket_file, in_favorite_file
+# from api.files.file_services import upload_file, download_file, delete_file, rename_file, get_all_files
 
 
 router = APIRouter()
 
 
 @router.post("/upload_file")
-def upload_file_endpoint(file: UploadFile, user_id: get_user_id = Depends()):
-    return {"file_id": upload_file(file, user_id)}
+async def upload_file_endpoint(file: UploadFile, user_id: get_user_id = Depends()):
+    return await upload_file(file, user_id)
 
 
 @router.get("/download_file/{file_id}")
-def download_file_endpoint(file_id: str, user_id: get_user_id = Depends()):
-    return download_file(file_id, user_id)
-
-
-# @router.patch("/in_basket_file/{file_id}")
-# def in_basket_file_endpoint(file_id: str, user_id: get_user_id = Depends()):
-#     return in_basket_file(file_id, user_id)
+async def download_file_endpoint(file_id: str, user_id: get_user_id = Depends()):
+    return await download_file(file_id, user_id)
 
 
 @router.patch("/rename_file/{file_id}/{new_name}")
-def rename_file_endpoint(file_id: str, new_name: str, user_id: get_user_id = Depends()):
-    return rename_file(file_id, new_name, user_id)
+async def rename_file_endpoint(file_id: str, new_name: str, user_id: get_user_id = Depends()):
+    return await rename_file(file_id, new_name, user_id)
 
 
-@router.get("/all_files")
-def get_all_files_endpoint(user_id: get_user_id = Depends()):
-    return get_all_files(user_id)
+@router.get("/delete_file/{file_id}")
+async def delete_file_endpoint(file_id: str, user_id: get_user_id = Depends()):
+    return await delete_file(file_id, user_id)
+
+
+@router.get("/in_basket_file/{file_id}")
+async def in_basket_file_endpoint(file_id: str, user_id: get_user_id = Depends()):
+    return await in_basket_file(file_id, user_id)
+
+
+@router.get("/in_favorite_file/{file_id}")
+async def in_favorite_file_endpoint(file_id: str, user_id: get_user_id = Depends()):
+    return await in_favorite_file(file_id, user_id)
