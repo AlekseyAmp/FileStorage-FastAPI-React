08871d547b24ed9c0727bc393c7bf6adcd8a5a4e
AlekseyAmp <thientaovv@gmail.com>
2023-04-06 20:52:35 +0500

    delete props

--- client/src/components/File/File.jsx
+++ client/src/components/File/File.jsx
@@ -1,19 +1,23 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import '../../assets/variables.scss';
 import styles from './File.module.scss'
 
 
-function File(props) {
-    const {name, size, image } = props;
-    return (
-      <div className={styles.file} onContextMenu={props.onContextMenu}>
-        <img src={image} alt="images" />
-        <div className={styles.fileText}>
-          <p className={`dark-text`}>{name}</p>
-          <p className={`small-text`}>Вес: {size}</p>
-        </div>
+function File({name, size, image, onContextMenu }) {
+  const [showSelectedFile, setShowSelectedFile] = useState(false);
+
+
+  
+  return (
+    <div className={styles.file}
+     onContextMenu={onContextMenu}>
+      <img src={image} alt="images" />
+      <div className={styles.fileText}>
+        <p className={`dark-text`}>{name}</p>
+        <p className={`small-text`}>Вес: {size}</p>
       </div>
-    );
+    </div>
+  );
 }
 
 export default File
