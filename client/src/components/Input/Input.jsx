706218fa14d94f70571ab85498884989408366ab
AlekseyAmp <thientaovv@gmail.com>
2023-04-06 20:53:20 +0500

    add new params in components

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 import styles from './Input.module.scss'
 
-function Input({title, placeholder, type, name}) {
+function Input({title, placeholder, type, name, value, onChange, onKeyDown}) {
   return (
     <div className={styles.inputForm}>
       <label htmlFor={name} className={`dark-text`}>{title}</label>
-      <input type={type} name={name} placeholder={placeholder} id={name} />
+
+      <input 
+        type={type} 
+        name={name} 
+        placeholder={placeholder} 
+        value={value} 
+        onChange={onChange} 
+        onKeyDown={onKeyDown} 
+        id={name} 
+      />
+
     </div>
   )
 }
