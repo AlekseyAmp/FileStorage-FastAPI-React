c66cddf1c0fbf89e7bbdac3b30aea6114d28bd45
AlekseyAmp <thientaovv@gmail.com>
2023-04-07 20:50:57 +0500

    file_services

--- server/src/services/file_services.py
+++ server/src/services/file_services.py
@@ -10,8 +10,6 @@ from models.file import File
 from constants.category_constants import ALLOWED_FORMATS
 
 
-
-
 def set_file_category(fileformat):
     for category in ALLOWED_FORMATS:
         if fileformat in ALLOWED_FORMATS[category]:
