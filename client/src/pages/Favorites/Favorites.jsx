97acae55558be757d2ad0f70726317a3b572f713
AlekseyAmp <thientaovv@gmail.com>
2023-04-07 13:40:10 +0500

    show all favorites file

--- client/src/pages/Favorites/Favorites.jsx
+++ client/src/pages/Favorites/Favorites.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "../../axios";
 
-import Category from '../../components/Category/Category'
+import File from "../../components/File/File";
 
-function Favorites() {
+import Cookies from "js-cookie";
+
+function Favoties() {
+  const [files, setFiles] = useState([]);
+  const access_token = Cookies.get('access_token')
+
+
+  useEffect(() => {
+    async function getFavoritesFiles() {
+      try {
+        const response = await axios.get("/get_favorites_files", {
+          headers: {
+            Authorization: `Bearer ${access_token}`
+          }
+        });
+        setFiles(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getFavoritesFiles();
+  }, []);
+
   return (
-    <Category
-    title={'Здесь хранятся ваши любимые файлы'}
-    titleIcon={<img src="img/categories/heart.png" alt="favorite" />}
-    labelTitle = {'по избранным файлам'}
-    background='rgb(196 255 229 / 60%)'
-    />
-  )
-}
+    <div>
+        {files.map((file) => (
+          <File
+          key={file.file_id}
+          onContextMenu={null}
+          image={`../img/categories/${file.content_type}.png`}
+          name={file.name}
+          size={`${Math.floor(file.size / 1000)} КБ`}
+        />
+        ))}
+    </div>
+  );
+};
 
-export default Favorites
+export default Favoties;
