d5fcd661d0abf53dfecef42c3a7f1e11244aa5e9
AlekseyAmp <thientaovv@gmail.com>
2023-04-07 20:41:37 +0500

    removed extra spaces

--- client/src/pages/Categories/Music.jsx
+++ client/src/pages/Categories/Music.jsx
@@ -5,7 +5,7 @@ import Category from '../../components/Category/Category'
 function Music() {
   return (
     <Category
-    categoryName={'music'}
+     categoryName={'/music'}
      title={'Музыка'}
      labelTitle = {'музыке'}
      background='rgb(220 227 245 / 60%)'
--- client/src/pages/Categories/Videos.jsx
+++ client/src/pages/Categories/Videos.jsx
@@ -5,7 +5,7 @@ import Category from '../../components/Category/Category'
 function Videos() {
   return (
     <Category
-    categoryName={'videos'}
+    categoryName={'/videos'}
     title={'Видео'}
     labelTitle = {'видео'}
     background='rgb(220 227 245 / 60%)'
