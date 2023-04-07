71de5e37812e67f132a7ff662808f9efc6130019
AlekseyAmp <thientaovv@gmail.com>
2023-04-07 21:22:43 +0500

    delete / from path

--- client/src/pages/Categories/Folders.jsx
+++ client/src/pages/Categories/Folders.jsx
@@ -5,7 +5,7 @@ import Category from '../../components/Category/Category'
 function Folders() {
   return (
     <Category
-    categoryName={'/folders'}
+    categoryName={'folders'}
     title={'Папки'}
     labelTitle = {'папкам'}
      background='rgb(220 227 245 / 60%)'
