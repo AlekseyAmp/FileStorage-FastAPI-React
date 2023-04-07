6a738967668273d200f9462b1598d69bde1272de
AlekseyAmp <thientaovv@gmail.com>
2023-04-07 20:39:04 +0500

    change alt for image

--- client/src/components/File/File.jsx
+++ client/src/components/File/File.jsx
@@ -3,11 +3,11 @@ import '../../assets/variables.scss';
 import styles from './File.module.scss'
 
 
-function File({name, size, image, onContextMenu }) {  
+function File({name, size, image, onContextMenu }) {
   return (
     <div className={styles.file}
      onContextMenu={onContextMenu}>
-      <img src={image} alt="images" />
+      <img src={image} alt="file" />
       <div className={styles.fileText}>
         <p className={`dark-text`}>{name}</p>
         <p className={`small-text`}>Вес: {size}</p>
