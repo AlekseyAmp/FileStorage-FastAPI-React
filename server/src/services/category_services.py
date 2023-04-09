df4f7618f1acbd18c8486aee53c23164273aa8a5
AlekseyAmp <thientaovv@gmail.com>
2023-04-09 10:25:24 +0500

    delete file_dict

--- server/src/services/category_services.py
+++ server/src/services/category_services.py
@@ -6,15 +6,6 @@ async def get_files_by_category(category: str, user_id: str):
     files = []
     async for file in File.find():
         if file.user_id == user_id and is_allowed_format(file.name.split('.')[1], category):
-            file_dict = {
-                "file_id": str(file.id),
-                "name": file.name,
-                "size": file.size,
-                "content_type": category,
-                "is_favorite": file.metadata["is_favorite"],
-                "is_deleted": file.metadata["is_deleted"],
-                "created_at": file.metadata["created_at"],
-            }
-            files.append(file_dict)        
+            files.append(file)        
     return files
 
