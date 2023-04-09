16559029b0b954d931d0d35cab84b48a0c86980a
AlekseyAmp <thientaovv@gmail.com>
2023-04-09 10:27:45 +0500

    include new router history

--- server/src/main.py
+++ server/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from routes import auth_routes, user_routes, file_routes, category_routes
+from routes import auth_routes, user_routes, file_routes, category_routes, history_routes
 
 from config.database import DbSettings
 
@@ -30,6 +30,7 @@ app.include_router(auth_routes.router, tags=['auth'], prefix='/api')
 app.include_router(user_routes.router, tags=['user'], prefix='/api')
 app.include_router(file_routes.router, tags=['file'], prefix='/api')
 app.include_router(category_routes.router, tags=['category'], prefix='/api')
+app.include_router(history_routes.router, tags=['history'], prefix='/api')
 
 db_settings = DbSettings()
 
@@ -45,27 +46,4 @@ if __name__ == "__main__":
 
 @app.get("/")
 def root():
-    return {"message": "go to /api"}
-
-
-@app.get("/api")
-def api_commands():
-    return {
-        "message": "Welcome to FastAPI with MongoDB",
-        "all query": "/docs (without /api)",
-        "register": "api/register",
-        "login": "api/login",
-        "refresh token": "api/refresh",
-        "logout": "api/logout",
-        "me": "api/me",
-        "upload file": "api/uploadfile",
-        "download file": "api/download_file/file_id",
-        "rename file": "api/rename_file/file_id",
-        "delete file": "api/delete_file/file_id",
-        "in_basket_file": "api/in_basket_file/file_id",
-        "in_favorite_file": "api/in_favorite_file/file_id",
-        "documents": "api/category/documents",
-        "images": "api/category/images",
-        "music": "api/category/music",
-        "videos": "api/category/videos",
-    }
+    return {"message": "go to /docs"}
