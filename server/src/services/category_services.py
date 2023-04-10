d84ac701ffed05fff66a5494e3a740e400afb6d0
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 00:48:22 +0500

    removed extra spaces

--- server/src/services/category_services.py
+++ server/src/services/category_services.py
@@ -4,8 +4,18 @@ from models.file import File
 
 async def get_files_by_category(category: str, user_id: str):
     files = []
-    async for file in File.find():
-        if file.user_id == user_id and is_allowed_format(file.name.split('.')[1], category):
-            files.append(file)        
+    async for file in File.find({
+        "user_id": user_id
+    }):
+        if is_allowed_format(file.name.split('.')[1], category):
+            file_dict = {
+                "file_id": str(file.id),
+                "name": file.name,
+                "size": file.size,
+                "content_type": category,
+                "is_favorite": file.metadata["is_favorite"],
+                "is_deleted": file.metadata["is_deleted"],
+                "created_at": file.metadata["created_at"],
+            }
+            files.append(file_dict)
     return files
-
