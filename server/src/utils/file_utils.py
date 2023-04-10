30b298993d4bbed452f3a8ce639907095323365f
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 00:50:33 +0500

    add method set file category

--- server/src/utils/file_utils.py
+++ server/src/utils/file_utils.py
@@ -1,30 +1,14 @@
-from fastapi import HTTPException
-from beanie import PydanticObjectId
-
 from constants.category_constants import ALLOWED_FORMATS
-from models.file import File
 
 
-async def get_file(file_id: str, user_id: str):
-    file = await File.get(PydanticObjectId(file_id))
-
-    if not file:
-        raise HTTPException(status_code=404, detail="Файл не найден")
-
-    if file.user_id != user_id:
-        raise HTTPException(status_code=403, detail="Отказано в доступе")
-
-    return file
-
-
-def set_file_category(fileformat):
+def set_file_category(fileformat: str):
     for category in ALLOWED_FORMATS:
         if fileformat in ALLOWED_FORMATS[category]:
             return category
     return False
 
 
-def is_allowed_format(fileformat, category):
+def is_allowed_format(fileformat: str, category: str):
     if fileformat in ALLOWED_FORMATS[category]:
         return True
     return False
