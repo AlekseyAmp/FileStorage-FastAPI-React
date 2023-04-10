afdd470636fcd565bae81fc7945cd779175ec772
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 00:44:30 +0500

    add mp3 format in music

--- server/src/constants/category_constants.py
+++ server/src/constants/category_constants.py
@@ -1,6 +1,7 @@
 ALLOWED_FORMATS = {
-    'documents': {'pdf','txt', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'},
-    'images': {'jpeg', 'png', 'gif', 'tiff', 'psd', 'eps', 'ai', 'indd', 'raw', 'jpg', 'webp', 'apng', 'avif', 'svg', 'bmp', 'ico'},
+    'documents': {'pdf', 'txt', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'},
+    'images': {'jpeg', 'png', 'gif', 'tiff', 'psd', 'eps', 'ai', 'indd', 'raw',
+               'jpg', 'webp', 'apng', 'avif', 'svg', 'bmp', 'ico'},
     'videos': {'mp4', 'avi', 'mov', 'wmv'},
-    'music': {'mp4', 'avi', 'mov', 'wmv'},
+    'music': {'mp3', 'mp4', 'avi', 'mov', 'wmv'},
 }
