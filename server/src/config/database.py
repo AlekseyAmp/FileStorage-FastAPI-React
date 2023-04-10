16c77e675830d28d1202a0cefb1f1ba95aaf61f7
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 00:38:22 +0500

    add new history document

--- server/src/config/database.py
+++ server/src/config/database.py
@@ -10,11 +10,11 @@ class DbSettings(BaseSettings):
     async def initialize_database(self):
         client = AsyncIOMotorClient(settings.DATABASE_URL)
         database = client[settings.DATABASE_NAME]
-        await init_beanie(database=database, document_models=[user.User, 
-                                                              file.File, 
-                                                              history.FileHistory, 
-                                                              history.UserHistory]
-                                                              )
+        await init_beanie(database=database,
+                          document_models=[user.User,
+                                           file.File,
+                                           history.FileHistory,
+                                           history.UserHistory])
 
     class Config:
         env_file = ".env"
