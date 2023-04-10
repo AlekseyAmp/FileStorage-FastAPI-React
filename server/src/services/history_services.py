13f106012d5eab661723ba7af8bfccb175e65de5
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 00:49:21 +0500

    add new service - history

--- server/src/services/history_services.py
+++ server/src/services/history_services.py
@@ -1,20 +1,60 @@
-from fastapi import HTTPException
-from datetime import datetime
+from typing import Any
+from constants.history_constants import today_date
+from models.history import FileHistory, UserHistory
 
 
-async def get_history(model, user_id: str):
-    history = []
-    async for history_elem in model.find():
-        if history_elem.user_id == user_id:
-             history.append(history_elem)
+async def get_files_history(user_id: str):
+    history = {}
+    async for history_elem in FileHistory.find({
+        "user_id": user_id
+    }):
+        date = history_elem.date
+        history[date] = []
+        for history_list_elem in history_elem.history_list:
+            history_dict = {
+                "history_id": str(history_elem.id),
+                "file_id": history_list_elem["file_id"],
+                "title": history_list_elem["title"],
+                "description": history_list_elem["description"],
+                "time": history_list_elem["time"],
+            }
+            history[date].append(history_dict)
     return history
 
 
-async def get_history_files_today(model, user_id: str):
-    today = datetime.now().strftime("%d-%m-%Y")
-    history_elem = await model.find_one({'user_id': user_id, 'date': today})
-    
+async def get_users_history(user_id: str):
+    history = {}
+    async for history_elem in UserHistory.find({
+        "user_id": user_id
+    }):
+        date = history_elem.date
+        history[date] = []
+        for history_list_elem in history_elem.history_list:
+            history_dict = {
+                "history_id": str(history_elem.id),
+                "title": history_list_elem["title"],
+                "description": history_list_elem["description"],
+                "time": history_list_elem["time"],
+            }
+            history[date].append(history_dict)
+    return history
+
+
+async def set_history_today(model: Any, history_dict: dict, user_id: str):
+    history_elem = await model.find_one({
+        "user_id": user_id,
+        "date": today_date
+    })
+
     if not history_elem:
-        raise HTTPException(status_code=404, detail="Запись истории не найдена")
-    
+        new_history = model(
+            user_id=user_id,
+            history_list=[history_dict]
+        )
+        await new_history.insert()
+    else:
+        history_dict
+        history_elem.history_list.append(history_dict)
+        await history_elem.save()
+
     return history_elem
