820e08a8195b100f04bad0ea90fb8349a9b94899
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 00:45:08 +0500

    removed extra spaces

--- server/src/models/file.py
+++ server/src/models/file.py
@@ -1,13 +1,13 @@
 from typing import Any, Dict
 from beanie import Document
 
+
 class File(Document):
-    
     user_id: str
     name: str
+    size: float
     content_type: str
     path: str
-    size: float
     metadata: Dict[str, Any] = {}
 
     class Settings:
