e8630ed35a6de7d929b1e922eb2c379839951c60
AlekseyAmp <thientaovv@gmail.com>
2023-04-11 11:17:49 +0500

    delete value from input

--- client/src/components/SearchInput/SearchInput.jsx
+++ client/src/components/SearchInput/SearchInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import styles from './SearchInput.module.scss'
 
@@ -6,7 +6,7 @@ function SearchInput({ title }) {
   return (
     <div className={styles.inputForm}>
       <label htmlFor="search" className={`dark-text`}>{title}</label>
-      <input type="text" placeholder='Поиск' onChange={handleInputChange} />
+      <input type="text" placeholder='Поиск' />
     </div>
   )
 }
