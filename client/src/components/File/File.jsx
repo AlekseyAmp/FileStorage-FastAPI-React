80e31d09ab9ffea4007cf0ba29b3c781e837d5ee
AlekseyAmp <thientaovv@gmail.com>
2023-04-14 20:04:27 +0500

    Now file names are limited to 5 characters, to display in full you need to click on three dots

--- client/src/components/File/File.jsx
+++ client/src/components/File/File.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../../assets/variables.scss';
-import styles from './File.module.scss'
+import styles from './File.module.scss';
 
-function File({name, size, image, onContextMenu }) {
+function File({ name, size, image, onContextMenu }) {
+  const [showFullFileName, setShowFullFileName] = useState(false);
+
+  const fileExtension = name.slice(name.lastIndexOf('.') + 1);
+ 
+  function handleNameClick() {
+    setShowFullFileName(!showFullFileName);
+  }
+
   return (
-    <div className={styles.file}
-     onContextMenu={onContextMenu}>
+    <div className={styles.file} onContextMenu={onContextMenu}>
       <img src={image} alt="file" />
       <div className={styles.fileText}>
-        <p className={`dark-text`}>{name}</p>
+        <p className={`dark-text`} onClick={handleNameClick}>
+          {showFullFileName ? name : (
+            <>
+              {name.slice(0, 5)}
+              <span className={`bold-text`}>...</span>
+              {fileExtension && `.${fileExtension}`}
+            </>
+          )}
+        </p>
         <p className={`small-text`}>Вес: {size}</p>
       </div>
     </div>
   );
 }
 
-export default File
+export default File;
