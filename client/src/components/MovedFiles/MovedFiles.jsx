38336aaeaacd2dfd3ba13ec2569411a4ba9201d3
AlekseyAmp <thientaovv@gmail.com>
2023-04-18 11:27:34 +0500

    Added context menu to Moved Files, added file deletion logic

--- client/src/components/MovedFiles/MovedFiles.jsx
+++ client/src/components/MovedFiles/MovedFiles.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import axios from '../../axios';
+
+import axios from '../../utils/axios';
+import { useContextMenu } from '../../utils/contextMenu'
+import {
+    downloadFile,
+    deleteFile,
+    renameFile
+} from '../../utils/fileActions';
 import Cookie from 'js-cookie';
 
 import '../../assets/variables.scss';
 
 import File from '../File/File';
+import Input from '../Input/Input'
 import SearchInput from '../SearchInput/SearchInput';
 import styles from './MovedFiles.module.scss';
 
-function MovedFiles({ url, title, titleIcon, labelTitle, background }) {
+
+
+function MovedFiles({ url, title, titleIcon, labelTitle, background, contextMenuBack, isFavorite }) {
 
     const categoryStyle = {
         background: background,
@@ -16,7 +26,22 @@ function MovedFiles({ url, title, titleIcon, labelTitle, background }) {
 
     const [files, setFiles] = useState([]);
 
-    const access_token = Cookie.get('access_token')
+    const {
+        handleContextMenu,
+        handleCloseContextMenu,
+        selectedFile,
+        showContextMenu,
+        contextMenuPosition,
+    } = useContextMenu();
+
+    const [showRenameInput, setShowRenameInput] = useState(false);
+
+    const access_token = Cookie.get('access_token');
+
+    async function handleRenameInput(e) {
+        e.preventDefault();
+        setShowRenameInput(selectedFile.name.split('.')[0]);
+    }
 
     useEffect(() => {
         async function getMovedFiles() {
@@ -42,14 +67,39 @@ function MovedFiles({ url, title, titleIcon, labelTitle, background }) {
                 {title}
             </div>
 
+            {showRenameInput && (
+                <Input
+                    value={showRenameInput}
+                    onChange={(e) => setShowRenameInput(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            renameFile(selectedFile, showRenameInput);
+                            setFiles(files.filter(file => file.file_id === selectedFile.file_id));
+                        }
+                    }}
+                />
+            )}
+
+            {showContextMenu && (
+                <div className={styles.contextMenu} onClick={handleCloseContextMenu} style={{ left: contextMenuPosition.x, top: contextMenuPosition.y }}>
+                    <div className={styles.contextMenuItem} >{contextMenuBack}</div>
+                    <div className={styles.contextMenuItem} onClick={() => downloadFile(selectedFile)}>Скачать</div>
+                    <div className={styles.contextMenuItem} onClick={handleRenameInput}>Переименовать</div>
+                    <div className={styles.contextMenuItem}>Поделиться</div>
+                    {isFavorite ? null : <div className={styles.contextMenuItem} onClick={() => deleteFile(selectedFile, setFiles, files)}>Удалить</div>}
+                </div>
+            )}
+
             <SearchInput title={`Поиск по ${labelTitle}`} />
 
             <div className={styles.files}>
                 {files.map((file) => (
                     <File
                         key={file.file_id}
+                        onContextMenu={(e) => handleContextMenu(e, file)}
                         image={`../img/categories/${file.content_type}.png`}
-                        name={file.name}
+                        name={file.name.split('.')[0]}
+                        extension={file.name.split('.')[1]}
                         size={`${Math.floor(file.size / 1000)} КБ`}
                     />
                 ))}
