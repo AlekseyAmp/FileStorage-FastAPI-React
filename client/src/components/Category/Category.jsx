7fc8fb77c3c1d94c82f35c2106a5ce22faf36fc8
AlekseyAmp <thientaovv@gmail.com>
2023-04-18 11:25:41 +0500

    Removed Context Menu and all functions with files and imported them from utils

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-import axios from '../../axios';
+import axios from '../../utils/axios';
+import { useContextMenu } from '../../utils/contextMenu'
+import {
+  downloadFile,
+  renameFile,
+  addToBasket,
+  addToFavorite
+} from '../../utils/fileActions';
+
 import Cookie from 'js-cookie';
 
 import '../../assets/variables.scss';
@@ -16,15 +24,23 @@ function Category({ url, title, titleIcon, labelTitle, background }) {
   };
 
   const [files, setFiles] = useState([]);
-  const [selectedFile, setSelectedFile] = useState(null);
 
-  const [showContextMenu, setShowContextMenu] = useState(false);
+  const {
+    handleContextMenu,
+    handleCloseContextMenu,
+    selectedFile,
+    showContextMenu,
+    contextMenuPosition,
+  } = useContextMenu();
 
-  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
   const [showRenameInput, setShowRenameInput] = useState(false);
 
   const access_token = Cookie.get('access_token');
 
+  async function handleRenameInput(e) {
+    e.preventDefault();
+    setShowRenameInput(selectedFile.name.split('.')[0]);
+  }
 
   useEffect(() => {
     async function getCategoryFiles() {
@@ -42,84 +58,6 @@ function Category({ url, title, titleIcon, labelTitle, background }) {
     getCategoryFiles();
   }, []);
 
-  async function handleContextMenu(e, file) {
-    e.preventDefault();
-    setSelectedFile(file);
-    setShowContextMenu(true);
-    setContextMenuPosition({ x: e.pageX, y: e.pageY });
-  }
-
-  async function handleCloseContextMenu(e) {
-    e.preventDefault();
-    setShowContextMenu(false);
-  }
-
-  async function handleDownload() {
-    try {
-      const response = await axios.get(`files/download/${selectedFile.file_id}`, {
-        headers: {
-          'Authorization': `Bearer ${access_token}`
-        },
-        responseType: 'blob'
-      });
-      const blob = new Blob([response.data], { type: selectedFile.content_type });
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', selectedFile.name);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link); 
-      window.URL.revokeObjectURL(url);
-    } catch (error) {
-      console.log(error.response.data.detail);
-    }
-  }
-  
-  async function handleRename(newName) {
-    try {
-      const response = await axios.patch(`files/rename/${selectedFile.file_id}/${newName}`, newName, {
-        headers: {
-          'Authorization': `Bearer ${access_token}`
-        }
-      });
-    } catch (error) {
-      console.log(error.response.data.detail);
-    }
-  }
-
-  async function handleRenameInput(e) {
-    e.preventDefault();
-    setShowRenameInput((selectedFile.name).split('.')[0]);
-  }
-
-  async function handleInBasket() {
-    try {
-      const response = await axios.patch(`files/in_basket/${selectedFile.file_id}`, {}, {
-        headers: {
-          'Authorization': `Bearer ${access_token}`
-        }
-      });
-      console.log(response.data);
-      setFiles(files.filter(file => file.file_id !== selectedFile.file_id));
-    } catch (error) {
-      console.log(error.response.data.detail);
-    }
-  }
-
-  async function handleInFavorite() {
-    try {
-      const response = await axios.patch(`files/in_favorite/${selectedFile.file_id}`, {}, {
-        headers: {
-          'Authorization': `Bearer ${access_token}`
-        }
-      });
-      console.log(response.data)
-    } catch (error) {
-      console.log(error.response.data.detail);
-    }
-  }
-
   return (
     <div className={styles.category} style={categoryStyle} onClick={handleCloseContextMenu}>
       <div className={`title ${styles.categoryTitle}`}>
@@ -134,7 +72,7 @@ function Category({ url, title, titleIcon, labelTitle, background }) {
             onChange={(e) => setShowRenameInput(e.target.value)}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
-                handleRename(showRenameInput);
+                renameFile(selectedFile, showRenameInput);
                 setFiles(files.filter(file => file.file_id === selectedFile.file_id));
               }
             }}
@@ -143,17 +81,17 @@ function Category({ url, title, titleIcon, labelTitle, background }) {
 
         {showContextMenu && (
           <div className={styles.contextMenu} onClick={handleCloseContextMenu} style={{ left: contextMenuPosition.x, top: contextMenuPosition.y }}>
-            <div className={styles.contextMenuItem} onClick={handleDownload}>Скачать</div>
-            <div className={styles.contextMenuItem} onClick={handleInFavorite}>Добавить избранное</div>
+            <div className={styles.contextMenuItem} onClick={() => downloadFile(selectedFile)}>Скачать</div>
+            <div className={styles.contextMenuItem} onClick={() => addToFavorite(selectedFile, setFiles, files)}>Добавить избранное</div>
             <div className={styles.contextMenuItem} onClick={handleRenameInput}>Переименовать</div>
             <div className={styles.contextMenuItem}>Поделиться</div>
-            <div className={styles.contextMenuItem} onClick={handleInBasket}>В коризну</div>
+            <div className={styles.contextMenuItem} onClick={() => addToBasket(selectedFile, setFiles, files)}>В коризну</div>
           </div>
         )}
 
         <SearchInput title={`Поиск по ${labelTitle}`} />
-            
-        <div className={styles.files}>    
+
+        <div className={styles.files}>
           {files.map((file) => {
             if (file.is_deleted) {
               return null;
@@ -163,7 +101,8 @@ function Category({ url, title, titleIcon, labelTitle, background }) {
                 key={file.file_id}
                 onContextMenu={(e) => handleContextMenu(e, file)}
                 image={`../img/categories/${file.content_type}.png`}
-                name={file.name}
+                name={file.name.split('.')[0]}
+                extension={file.name.split('.')[1]}
                 size={`${Math.floor(file.size / 1000)} КБ`}
               />
             );
