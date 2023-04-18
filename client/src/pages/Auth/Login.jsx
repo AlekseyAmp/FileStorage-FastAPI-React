8531849727e356377dcec17a902f44c7cc65f8b6
AlekseyAmp <thientaovv@gmail.com>
2023-04-18 11:28:15 +0500

    Change utils import

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import axios from '../../axios';
+import axios from '../../utils/axios';
 import Cookie from 'js-cookie'
 
 import AuthForm from '../../components/AuthForm/AuthForm';
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import axios from '../../axios';
+import axios from '../../utils/axios';
 
 import AuthForm from '../../components/AuthForm/AuthForm';
 import styles from './Auth.module.scss';
