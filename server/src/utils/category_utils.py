a78a7e5336f44a6ca6e5c4b4cd4e26e6f16caec6
AlekseyAmp <thientaovv@gmail.com>
2023-04-20 21:15:35 +0500

    Added category utilities

---
+++ server/src/utils/category_utils.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+from beanie import PydanticObjectId
+
+from models.category import Category
+from constants.category_constants import ALLOWED_FORMATS
+
+
+async def get_category(category_id: str, user_id: str):
+    category = await Category.get(PydanticObjectId(category_id))
+
+    if not category:
+        raise HTTPException(
+            status_code=404,
+            detail="Категория не найдена"
+        )
+
+    if category.user_id != user_id:
+        raise HTTPException(
+            status_code=403,
+            detail="Отказано в доступе"
+        )
+
+    return category
+
+
+def set_file_category(fileformat: str):
+    for category in ALLOWED_FORMATS:
+        if fileformat in ALLOWED_FORMATS[category]:
+            return category
+    return False
