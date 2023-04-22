746b78e34d2a0f0cecd2c2229b30f41ce952a3cb
AlekseyAmp <thientaovv@gmail.com>
2023-04-22 22:38:51 +0500

    Now will display the entire history, and the most recent actions will be saved in the database from top to bottom

--- server/src/services/history_services.py
+++ server/src/services/history_services.py
@@ -1,38 +1,46 @@
-from typing import Any
 from datetime import datetime
-from models.history import FileHistory, UserHistory
+from models.history import History
 
 
-async def get_files_history(user_id: str):
+async def get_all_history(user_id: str):
     history = {}
-    async for history_elem in FileHistory.find({
+    async for history_elem in History.find({
         "user_id": user_id
     }):
         date = history_elem.date
         history[date] = []
         for history_list_elem in history_elem.history_list:
+            if "default_category" in history_list_elem["description"]:
+                continue
+
             history_dict = {
-                "file_id": history_list_elem["file_id"],
                 "title": history_list_elem["title"],
                 "description": history_list_elem["description"],
                 "time": history_list_elem["time"]
             }
-            history[date].insert(0, history_dict)
+            history[date].append(history_dict)
 
     return history
 
 
-async def get_last_five_files_history(user_id: str):
+async def get_last_five_history(user_id: str):
     history = []
-    async for history_elem in FileHistory.find({
+    async for history_elem in History.find({
         "user_id": user_id,
         "date": datetime.now().strftime("%d-%m-%Y"),
     }):
-        for history_list_elem in history_elem.history_list[-1:-6:-1]:
+        for history_list_elem in history_elem.history_list[0:6]:
+            if history_list_elem.get("category_name"):
+                if history_list_elem["category_name"] == "default_category":
+                    continue
+                name = history_list_elem["category_name"]
+            elif history_list_elem.get("file_name"):
+                name = history_list_elem["file_name"]
+            else:
+                name = history_list_elem["title"]
+
             history_dict = {
-                "file_id": history_list_elem["file_id"],
-                "file_name": history_list_elem["file_name"],
-                "file_contentType": history_list_elem["file_contentType"],
+                "name": name,
                 "title": history_list_elem["title"],
                 "description": history_list_elem["description"],
                 "date": history_elem.date,
@@ -43,38 +51,20 @@ async def get_last_five_files_history(user_id: str):
     return history
 
 
-async def get_user_history(user_id: str):
-    history = {}
-    async for history_elem in UserHistory.find({
-        "user_id": user_id
-    }):
-        date = history_elem.date
-        history[date] = []
-        for history_list_elem in history_elem.history_list:
-            history_dict = {
-                "title": history_list_elem["title"],
-                "description": history_list_elem["description"],
-                "time": history_list_elem["time"]
-            }
-            history[date].insert(0, history_dict)
-
-    return history
-
-
-async def set_history_today(model: Any, history_dict: dict, user_id: str):
-    history_elem = await model.find_one({
+async def set_history_today(history_dict: dict, user_id: str):
+    history_elem = await History.find_one({
         "user_id": user_id,
         "date": datetime.now().strftime("%d-%m-%Y")
     })
 
     if not history_elem:
-        new_history = model(
+        new_history = History(
             user_id=user_id,
             history_list=[history_dict]
         )
         await new_history.insert()
     else:
-        history_elem.history_list.append(history_dict)
+        history_elem.history_list.insert(0, history_dict)
         await history_elem.save()
 
     return history_elem
