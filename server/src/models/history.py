c2e74083262c7eea1bd3cb03a42fad9268043356
AlekseyAmp <thientaovv@gmail.com>
2023-04-22 22:33:05 +0500

    Now there is one model to store all the history - History

--- server/src/models/history.py
+++ server/src/models/history.py
@@ -3,28 +3,10 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 
-class FileHistory(Document):
+class History(Document):
     user_id: str
     date: str = datetime.now().strftime("%d-%m-%Y")
     history_list: List[Dict[str, Any]] = []
 
     class Settings:
-        name = "files_history"
-
-
-class CategoryHistory(Document):
-    user_id: str
-    date: str = datetime.now().strftime("%d-%m-%Y")
-    history_list: List[Dict[str, Any]] = []
-
-    class Settings:
-        name = "categories_history"
-
-
-class UserHistory(Document):
-    user_id: str
-    date: str = datetime.now().strftime("%d-%m-%Y")
-    history_list: List[Dict[str, Any]] = []
-
-    class Settings:
-        name = "users_history"
+        name = "history"
