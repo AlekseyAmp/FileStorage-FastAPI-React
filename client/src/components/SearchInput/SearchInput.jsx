88de18aa47366a398ddab7731556bf8b0180c690
AlekseyAmp <thientaovv@gmail.com>
2023-04-24 15:46:53 +0500

    Make working search

--- client/src/components/SearchInput/SearchInput.jsx
+++ client/src/components/SearchInput/SearchInput.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-import styles from './SearchInput.module.scss'
+import styles from './SearchInput.module.scss';
 
-function SearchInput({ title }) {
+function SearchInput({ title, onSearch }) {
+  const handleChange = (event) => {
+    onSearch(event.target.value);
+  };
+
   return (
     <div className={styles.inputForm}>
-      <label htmlFor="search" className={`dark-text`}>{title}</label>
-      <input type="text" placeholder='Поиск' />
+      <label htmlFor="search" className={`dark-text`}>
+        {title}
+      </label>
+      <input type="text" placeholder="Поиск" onChange={handleChange} />
     </div>
-  )
+  );
 }
 
 export default SearchInput;
