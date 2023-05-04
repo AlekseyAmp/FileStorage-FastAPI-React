ab8af7dc4aedc78b70b4b98e2a9da9f90fbbb3b9
AlekseyAmp <thientaovv@gmail.com>
2023-05-05 00:56:16 +0500

    file information and actions to add to cart or favorite are now here

--- server/src/services/file_services.py
+++ server/src/services/file_services.py
@@ -1,6 +1,5 @@
 from fastapi import HTTPException, UploadFile
 from beanie import PydanticObjectId
-from fastapi.responses import FileResponse
 from datetime import datetime
 import urllib
 import os
@@ -16,6 +15,39 @@ from utils.file_utils import get_file
 from utils.category_utils import set_file_category
 
 
+async def get_all_files_info(user_id: str):
+    files_info_dict = {
+        "total_count": 0,
+        "total_size": 0
+    }
+
+    async for file in File.find({
+        "user_id": user_id,
+    }):
+        files_info_dict["total_count"] += 1
+        files_info_dict["total_size"] += file.size
+
+    return files_info_dict
+
+
+async def get_files_info_by_category(category_name: str, user_id: str):
+    files_info_dict = {
+        "total_count": 0,
+        "total_size": 0
+    }
+
+    async for file in File.find({
+        "user_id": user_id,
+        "category_name": category_name,
+        "metadata.is_basket": False,
+        "metadata.is_favorite": False
+    }):
+        files_info_dict["total_count"] += 1
+        files_info_dict["total_size"] += file.size
+
+    return files_info_dict
+
+
 async def create_new_file(file: UploadFile, category_name: str, user_id: str):
     user = await User.get(PydanticObjectId(user_id))
 
@@ -27,8 +59,11 @@ async def create_new_file(file: UploadFile, category_name: str, user_id: str):
 
     category_name = category_name.lower()
 
-    # Default categories(images, documents, music, videos) are abstract,
-    # all files associated with them are in the DEFAULT_CATEGORY folder
+    """
+    Default categories(images, documents, music, videos) are abstract,
+    All files associated with them are in the DEFAULT_CATEGORY folder
+    """
+
     default_categories = ("images", "documents", "music", "videos")
 
     if category_name in default_categories:
@@ -85,6 +120,14 @@ async def create_new_file(file: UploadFile, category_name: str, user_id: str):
     await user.update({"$inc":
                       {f"metadata.storage_used": file.size}})
 
+    """
+    Block code 99-100
+
+    There is no need to set the default category sizes,
+    they are set by the function (get_files_info_by_category in info_services.py),
+    but the categories that the user creates must be changed using the function
+    (change_size_category)
+    """
     if category_name not in default_categories:
         await change_size_category(category_name, "upload", file.size, user_id)
 
@@ -98,7 +141,7 @@ async def create_new_file(file: UploadFile, category_name: str, user_id: str):
         "time": datetime.now().strftime("%H:%M:%S")
     }
     await set_history_today(history_dict, user_id)
-    
+
     await set_statistic_today("upload", user_id)
     return {"file_id": new_file.id}
 
@@ -155,6 +198,130 @@ async def rename_file(file_id: str, new_name: str, user_id: str):
     await set_history_today(history_dict, user_id)
 
     return {"new_name": new_name}
+
+
+async def add_to_basket_file(file_id: str, user_id: str):
+    file = await get_file(file_id, user_id)
+
+    if file.metadata["is_favorite"]:
+        raise HTTPException(
+            status_code=403,
+            detail="Файл находится в избранном"
+        )
+
+    if file.metadata["is_basket"]:
+        raise HTTPException(
+            status_code=403,
+            detail="Файл уже находится в корзине"
+        )
+
+    default_categories = ("images", "documents", "music", "videos")
+
+    category_name = file.category_name
+
+    if category_name in default_categories:
+        category_name = "default_category"
+
+    history_dict = {
+        "file_id": str(file_id),
+        "file_name": file.name,
+        "file_content_type": file.content_type,
+        "file_category_name": file.category_name,
+        "title": "Добавление файла в корзину",
+        "description": f"Файл {file.name} был перемещён в корзину",
+        "time": datetime.now().strftime("%H:%M:%S")
+    }
+
+    await file.update({"$set": {"metadata.is_basket": True}})
+
+    await change_size_category(category_name, "delete", file.size, user_id)
+
+    await set_history_today(history_dict, user_id)
+
+    return {"status": "succes"}
+
+
+async def add_to_favorite_file(file_id: str, user_id: str):
+    file = await get_file(file_id, user_id)
+
+    if file.metadata["is_basket"]:
+        raise HTTPException(
+            status_code=403,
+            detail="Файл находится в корзине"
+        )
+
+    if file.metadata["is_favorite"]:
+        raise HTTPException(
+            status_code=403,
+            detail="Файл уже находится в избранном"
+        )
+
+    default_categories = ("images", "documents", "music", "videos")
+
+    category_name = file.category_name
+
+    if category_name in default_categories:
+        category_name = "default_category"
+
+    history_dict = {
+        "file_id": str(file_id),
+        "file_name": file.name,
+        "file_content_type": file.content_type,
+        "file_category_name": file.category_name,
+        "title": "Добавление файла в избранное",
+        "description": f"Файл {file.name} был перемещён в избранное",
+        "time": datetime.now().strftime("%H:%M:%S")
+    }
+
+    await file.update({"$set": {"metadata.is_favorite": True}})
+
+    await change_size_category(category_name, "delete", file.size, user_id)
+
+    await set_history_today(history_dict, user_id)
+
+    return {"status": "succes"}
+
+
+async def revert_moved_file_back(file_id: str, user_id: str):
+    file = await get_file(file_id, user_id)
+
+    file_location = ''
+
+    if file.metadata["is_basket"]:
+        await file.update({"$set": {"metadata.is_basket": False}})
+        file_location = "корзины"
+
+    elif file.metadata["is_favorite"]:
+        await file.update({"$set": {"metadata.is_favorite": False}})
+        file_location = "избранного"
+
+    elif (not file.metadata["is_favorite"]) and (not file.metadata["is_basket"]):
+        raise HTTPException(
+            status_code=403,
+            detail="Файл не находится ни в корзине, ни в избранном"
+        )
+
+    default_categories = ("images", "documents", "music", "videos")
+
+    category_name = file.category_name
+
+    if category_name in default_categories:
+        category_name = "default_category"
+
+    history_dict = {
+        "file_id": str(file_id),
+        "file_name": file.name,
+        "file_contentType": file.content_type,
+        "file_categoryName": file.category_name,
+        "title": f"Удаление файла из {file_location}",
+        "description": f"Файл {file.name} был убран из {file_location}",
+        "time": datetime.now().strftime("%H:%M:%S")
+    }
+    await set_history_today(history_dict, user_id)
+
+    await change_size_category(category_name, "upload", file.size, user_id)
+
+    return {"status": "succes"}
 
 
 async def delete_file(file_id: str, user_id: str):
