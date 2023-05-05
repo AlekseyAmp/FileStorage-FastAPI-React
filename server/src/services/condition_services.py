a943305317190a6917fe20d2ae7fe7c1e524e1f8
AlekseyAmp <thientaovv@gmail.com>
2023-05-05 11:22:05 +0500

    Remove extra input

--- server/src/services/condition_services.py
+++ server/src/services/condition_services.py
@@ -1,10 +1,4 @@
-from fastapi import HTTPException
-from datetime import datetime
-
 from models.file import File
-from services.history_services import set_history_today
-from services.category_services import change_size_category
-from utils.file_utils import get_file
 
 
 async def get_moved_files(condition: str, user_id: str):
