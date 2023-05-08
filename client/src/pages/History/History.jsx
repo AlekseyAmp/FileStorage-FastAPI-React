7871d41d7041104d9fe22950a77af3ab16009636
AlekseyAmp <thientaovv@gmail.com>
2023-05-08 23:06:31 +0500

    Add search for history

--- client/src/pages/History/History.jsx
+++ client/src/pages/History/History.jsx
@@ -11,44 +11,60 @@ import SearchInput from '../../components/SearchInput/SearchInput';
 function History() {
   const [history, setHistory] = useState({});
   const access_token = Cookie.get('access_token');
-  
-  useEffect(() => {
-    async function getFilesHistory() {
-      try {
-        const response = await axios.get(`/history`, {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        });
-        setHistory(response.data);
-      } catch (error) {
-        console.log(error.response.data.detail);
-      }
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  async function getFilesHistory() {
+    try {
+      const response = await axios.get(`/history`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+      setHistory(response.data);
+    } catch (error) {
+      console.log(error.response.data.detail);
     }
+  }
+
+  useEffect(() => {
     getFilesHistory();
   }, []);
 
-  return (
-    <div className={styles.history} >
-      <div className={'title'}>История</div>
-      <SearchInput />
-      <div className={styles.historyContent}>
-        {Object.entries(history).map(([date, history_elems]) => (
+  const filteredHistory = {};
+  Object.entries(history).forEach(([date, historyElems]) => {
+    const filteredElems = historyElems.filter((elem) =>
+      elem.description.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    if (filteredElems.length > 0) {
+      filteredHistory[date] = filteredElems;
+    }
+  });
 
+  function handleSearch(query) {
+    setSearchQuery(query);
+  }
+
+  return (
+    <div className={styles.history}>
+      <div className={'title'}>История</div>
+      <SearchInput onSearch={handleSearch} />
+      <div className={styles.historyContent}>
+        {Object.entries(filteredHistory).map(([date, historyElems]) => (
           <div className={styles.historyElem} key={date}>
             <h3 className={'bold-text'}>Дата: {date}</h3>
 
-            {history_elems.map((history_elem, i) => (
+            {historyElems.map((historyElem, i) => (
               <div className={styles.historyElemContent} key={i + 1}>
                 <p className={'dark-text'}>
-                  {history_elem.time}
-                  <span className={`dark-text ${styles.historyElemDescription}`}>{history_elem.description}</span>
+                  {historyElem.time}
+                  <span className={`dark-text ${styles.historyElemDescription}`}>
+                    {historyElem.description}
+                  </span>
                 </p>
               </div>
-
             ))}
           </div>
-
         ))}
       </div>
     </div>
