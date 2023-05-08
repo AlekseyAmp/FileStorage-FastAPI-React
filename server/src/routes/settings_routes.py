6a97b427f5543de9f126c230b881466f6c8d88ba
AlekseyAmp <thientaovv@gmail.com>
2023-05-08 23:08:10 +0500

    Add password change endpoint

--- server/src/routes/settings_routes.py
+++ server/src/routes/settings_routes.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, Depends
 
-from services import user_services as u_s
+from services.user_services import get_user_id
+from services import settings_services as set_s
 
 
 router = APIRouter()
 
 
 @router.patch("/settings/email/{new_email}")
-async def change_email(new_email: str, user_id: u_s.get_user_id = Depends()):
-    return await u_s.change_email(user_id, new_email)
+async def change_email(new_email: str, user_id: get_user_id = Depends()):
+    return await set_s.change_email(user_id, new_email)
+
+
+@router.patch("/settings/password/{new_password}")
+async def change_password(new_password: str, user_id: get_user_id = Depends()):
+    return await set_s.change_password(user_id, new_password)
